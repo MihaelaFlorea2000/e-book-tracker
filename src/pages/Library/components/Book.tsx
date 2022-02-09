0ec9c7dcffcaec7a5e632dc860006210a7c2491b
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2022-02-09 22:09:48 +0200

    Display default cover if no cover exists

--- src/pages/Library/components/Book.tsx
+++ src/pages/Library/components/Book.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import {BookInterface} from "../../../config/interfaces";
 import { border, theme } from "../../../utils/style/themeConfig";
 import {DOMAIN} from "../../../config/config";
+// @ts-ignore
+import defaultCoverImage from "../../../utils/images/defaultCoverImage.jpeg";
 
 interface Props {
     book: BookInterface
@@ -10,10 +12,13 @@ interface Props {
 
 const Book = (props: Props) => {
 
+    const coverImage = props.book.coverImage !== null ? props.book.coverImage : defaultCoverImage;
+    const showTitle = props.book.coverImage !== null ? false : true;
+
     return (
         <BookContainer >
-            <BookImage image={`${DOMAIN}${props.book.coverImage}`} />
-            {props.book.title}
+            <BookImage image={coverImage} />
+            {showTitle && <Title>{props.book.title}</Title>}
         </BookContainer>
     )
 }
@@ -37,4 +42,15 @@ const BookImage = styled.div<{image: string}>`
   height: 220px;
   background-image: url(${props => props.image});
   background-size: cover;
+`
+
+const Title = styled.div`
+  position: absolute;
+  width: 130px;
+  height: 220px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: white;
 `
